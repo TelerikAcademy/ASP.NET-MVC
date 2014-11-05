8e2f4584427b2b6ffb895ae2de9f2eb47e549f59
ivaylokenov <ivaylo.kenov@gmail.com>
2014-11-05 16:47:56 +0200

    Session, TempData and Cache demos added

---
+++ 05. Working With Data/WorkingWithDataMvc/Controllers/MemoryStorageController.cs
@@ -0,0 +1,45 @@
+﻿namespace WorkingWithDataMvc.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class MemoryStorageController : Controller
+    {
+        public ActionResult Index()
+        {
+            var now = DateTime.Now;
+            if (this.HttpContext.Session["date"] == null)
+            {
+                this.HttpContext.Session["date"] = now;
+            }
+
+            var date = (DateTime)this.HttpContext.Session["date"];
+
+            return View(date);
+        }
+
+        public ActionResult SaveToTempData()
+        {
+            this.TempData["message"] = "TempData success!";
+            return RedirectToAction("Redirection");
+        }
+
+        public ActionResult Redirection()
+        {
+            var data = this.TempData["message"];
+            return View(data);
+        }
+
+        public ActionResult SaveToCache()
+        {
+            this.HttpContext.Cache["message"] = "Cache success!";
+            return RedirectToAction("RedirectForCache");
+        }
+
+        public ActionResult RedirectForCache()
+        {
+            var data = this.HttpContext.Cache["message"];
+            return View(data);
+        }
+    }
+}
