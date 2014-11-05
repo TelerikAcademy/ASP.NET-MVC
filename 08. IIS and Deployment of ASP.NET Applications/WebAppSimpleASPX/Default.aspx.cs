1563e5412c122225d4a160d7ec169156371f3fc9
NikolayIT <admin@nikolay.it>
2014-11-05 18:03:56 +0200

    StyleCop warnings fixed in IIS deployment sample app

--- 08. IIS and Deployment of ASP.NET Applications/WebAppSimpleASPX/Default.aspx.cs
+++ 08. IIS and Deployment of ASP.NET Applications/WebAppSimpleASPX/Default.aspx.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
-namespace WebAppSimpleASPX
+﻿namespace WebAppSimpleASPX
 {
+    using System;
+
     public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void ButtonGreetings_Click(object sender, EventArgs e)
