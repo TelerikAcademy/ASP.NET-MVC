10450931d3deff5625e1f08fccee9ff84e3a8116
ivaylokenov <ivaylo.kenov@gmail.com>
2014-11-05 09:22:32 +0200

    Model binding of normal data object

--- 05. Working With Data/WorkingWithDataMvc/Controllers/ModelBindingController.cs
+++ 05. Working With Data/WorkingWithDataMvc/Controllers/ModelBindingController.cs
@@ -1,6 +1,7 @@
 ﻿namespace WorkingWithDataMvc.Controllers
 {
     using System.Web.Mvc;
+    using WorkingWithDataMvc.Models;
 
     public class ModelBindingController : Controller
     {
@@ -13,7 +14,19 @@
         [ValidateAntiForgeryToken]
         public ActionResult Parameter(int first, string second, bool third)
         {
-            TempData["Success"] = string.Format("{0} {1} {2}", first, second, third);
+            return this.RedirectToAction(string.Format("{0} {1} {2}", first, second, third));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Object(PersonViewModel person)
+        {
+            return this.SetTempDataAndRedirectToAction(string.Format("{0} {1} {2}", person.FirstName, person.LastName, person.Age));
+        }
+
+        private ActionResult SetTempDataAndRedirectToAction(string msg)
+        {
+            TempData["Success"] = msg;
             return RedirectToAction("Index");
         }
     }
---
+++ 05. Working With Data/WorkingWithDataMvc/Models/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkingWithDataMvc.Models
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
