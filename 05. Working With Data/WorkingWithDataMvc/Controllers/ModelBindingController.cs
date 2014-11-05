b092b74b869137427b30ed54ca6f0ee83702d827
ivaylokenov <ivaylo.kenov@gmail.com>
2014-11-05 09:03:15 +0200

    Parameter binding added to demos

---
+++ 05. Working With Data/WorkingWithDataMvc/Controllers/ModelBindingController.cs
@@ -0,0 +1,20 @@
+﻿namespace WorkingWithDataMvc.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ModelBindingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Parameter(int first, string second, bool third)
+        {
+            TempData["Success"] = string.Format("{0} {1} {2}", first, second, third);
+            return RedirectToAction("Index");
+        }
+    }
+}
