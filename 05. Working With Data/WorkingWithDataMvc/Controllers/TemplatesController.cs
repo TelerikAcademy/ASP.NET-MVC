55e1bbf2bad3f26e75f47a34b7d416a1412102c0
ivaylokenov <ivaylo.kenov@gmail.com>
2014-11-05 12:33:38 +0200

    Display and Editor templates demos added

---
+++ 05. Working With Data/WorkingWithDataMvc/Controllers/TemplatesController.cs
@@ -0,0 +1,19 @@
+﻿namespace WorkingWithDataMvc.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using WorkingWithDataMvc.Models;
+
+    public class TemplatesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new IndexViewModel
+            {
+                Text = "Some Text",
+                Number = 15,
+                Date = DateTime.Now
+            });
+        }
+    }
+}
---
+++ 05. Working With Data/WorkingWithDataMvc/Models/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkingWithDataMvc.Models
+{
+    public class IndexViewModel
+    {
+        public string Text { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
