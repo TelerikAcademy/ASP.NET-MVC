d9b52bbd60998a06fa15b7f58014722adc9d41e7
NikolayIT <admin@nikolay.it>
2014-11-05 17:52:22 +0200

    Fixed StyleCop warnings in ASP.NET Caching data demos

--- 07. ASP.NET Caching Data/Caching/Site.Master.cs
+++ 07. ASP.NET Caching Data/Caching/Site.Master.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace Caching
+﻿namespace Caching
 {
+    using System;
+    using System.Web.UI;
+
     public partial class SiteMaster : MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
     }
 }
--- 07. ASP.NET Caching Data/Caching/ViewSwitcher.ascx.cs
+++ 07. ASP.NET Caching Data/Caching/ViewSwitcher.ascx.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Routing;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using Microsoft.AspNet.FriendlyUrls.Resolvers;
-
 namespace Caching
 {
+    using System;
+    using System.Web;
+    using System.Web.Routing;
+
+    using Microsoft.AspNet.FriendlyUrls.Resolvers;
+
     public partial class ViewSwitcher : System.Web.UI.UserControl
     {
         protected string CurrentView { get; private set; }
@@ -21,10 +18,10 @@ namespace Caching
         {
             // Determine current view
             var isMobile = WebFormsFriendlyUrlResolver.IsMobileView(new HttpContextWrapper(Context));
-            CurrentView = isMobile ? "Mobile" : "Desktop";
+            this.CurrentView = isMobile ? "Mobile" : "Desktop";
 
             // Determine alternate view
-            AlternateView = isMobile ? "Desktop" : "Mobile";
+            this.AlternateView = isMobile ? "Desktop" : "Mobile";
 
             // Create switch URL from the route, e.g. ~/__FriendlyUrls_SwitchView/Mobile?ReturnUrl=/Page
             var switchViewRouteName = "AspNet.FriendlyUrls.SwitchView";
@@ -35,9 +32,10 @@ namespace Caching
                 this.Visible = false;
                 return;
             }
-            var url = GetRouteUrl(switchViewRouteName, new { view = AlternateView, __FriendlyUrls_SwitchViews = true });
+
+            var url = GetRouteUrl(switchViewRouteName, new { view = this.AlternateView, __FriendlyUrls_SwitchViews = true });
             url += "?ReturnUrl=" + HttpUtility.UrlEncode(Request.RawUrl);
-            SwitchUrl = url;
+            this.SwitchUrl = url;
         }
     }
 }
